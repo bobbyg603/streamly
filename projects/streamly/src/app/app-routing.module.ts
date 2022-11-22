a563b1907c5c0d9106c9c542e3d8514cf45aac84
Bobby Galli <bobbyg603@gmail.com>
2022-11-22 13:43:17 -0500

    fix: block watch page if not authenticated

--- projects/streamly/src/app/app-routing.module.ts
+++ projects/streamly/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { AuthGuardService } from './core/core.module';
 
 const routes: Routes = [
   {
@@ -14,6 +15,7 @@ const routes: Routes = [
   },
   {
     path: 'watch',
+    canActivate: [AuthGuardService],
     loadChildren: () =>
       import('./features/watch/watch.module').then((m) => m.WatchModule)
   },
--- projects/streamly/src/app/core/auth/auth-guard.service.ts
+++ projects/streamly/src/app/core/auth/auth-guard.service.ts
@@ -1,18 +1,30 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
-import { Store, select } from '@ngrx/store';
+import { CanActivate, Router } from '@angular/router';
+import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 
-import { selectIsAuthenticated } from './auth.selectors';
+import { switchMap } from 'rxjs/operators';
 import { AppState } from '../core.state';
+import { selectIsAuthenticated } from './auth.selectors';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardService implements CanActivate {
-  constructor(private store: Store<AppState>) {}
+  constructor(private router: Router, private store: Store<AppState>) {}
 
   canActivate(): Observable<boolean> {
-    return this.store.pipe(select(selectIsAuthenticated));
+    return this.store.pipe(
+      select(selectIsAuthenticated),
+      switchMap(async (authenticated) => {
+        // https://github.com/angular/angular/issues/16211#issuecomment-296526518
+        if (!authenticated) {
+          await this.router.navigate(['']);
+          return false;
+        }
+
+        return true;
+      })
+    );
   }
 }
